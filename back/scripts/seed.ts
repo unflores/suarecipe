a883934b444e1e64d7c216d1080f9cb0fa1f8c2a
Austin Flores <ctrl4ltdeleteme@gmail.com>
2018-11-27 22:57:33 +0000

    Fix the seed file

--- back/scripts/seed.ts
+++ back/scripts/seed.ts
@@ -5,14 +5,7 @@ import {createReadStream} from 'fs'
 import * as path from 'path'
 import Location from '../models/location'
 import { ValidationError, Document } from 'mongoose'
-import * as BlueBird from 'bluebird'
-const Promise = BlueBird.Promise
-
-const fileName    = path.resolve(__dirname, './masterList.csv')
-const parser      = parse({delimiter: ',', columns: true})
-const fileStream  = createReadStream(fileName)
-const tasks: BlueBird<void | Document>[]  = []
-const errors: Error[]   = []
+import * as Bluebird from 'bluebird'
 
 interface IImportedLocation {
   Name: string
@@ -23,35 +16,59 @@ interface IImportedLocation {
   zipcode: string
 }
 
+class MasterListHandler {
 
-fileStream.pipe(parser).on('data', function(imported: IImportedLocation) {
+  tasks: Bluebird<void | Document>[]
+  errors: string[]
 
-  let location = new Location({
-    name:         imported.Name,
-    description:  imported.Description,
-    type:         imported.Type,
-    siteLink:     imported.website,
-    address:      imported.address,
-    zipcode:      imported.zipcode
-  })
+  constructor(){
+    this.tasks  = []
+    this.errors = []
+  }
 
-  tasks.push(location.save().catch((err: ValidationError) => {
-    console.log('<<Error:>> name: ', imported.Name, 'website: ', imported.website)
-    console.log(err.message)
-    errors.push(err)
-  }))
+  handleData = (imported: IImportedLocation): void => {
+    let location = new Location({
+      name:         imported.Name,
+      description:  imported.Description,
+      partsOfDay:    imported['Day Part'].split(','),
+      type:         imported.Type,
+      siteLink:     imported.website,
+      address:      imported.address,
+      zipcode:      imported.zipcode
+    })
 
+    this.tasks.push(location.save().catch((err: ValidationError) => {
+      const objectErrors: string[] = []
+      objectErrors.push(`listing: ${imported.Name}`)
+      Object.entries(err).forEach(
+        ([key, value]) => objectErrors.push(`${key} : ${value}`)
+      )
+      objectErrors.push('================')
 
-}).on('end', function() {
-  Promise.all(tasks).then((data) => {
-    console.log('total rows: ', tasks.length)
-    console.log('total errors: ', errors.length)
-    console.log('total saved: ', tasks.length - errors.length)
-  }).catch((error) => {
-    console.log('Problem loading data', error)
-  }).finally(() => { process.exit() })
-})
+      this.errors.push(objectErrors.join("\n"))
+    }))
+  }
 
+  handleEnd = (): void => {
+    Bluebird.all(this.tasks).then((data) => {
+      console.log('errors: ', this.errors.join("\n"))
+      console.log('total rows: ', this.tasks.length)
+      console.log('total errors: ', this.errors.length)
+      console.log('total saved: ', this.tasks.length - this.errors.length)
+    }).catch((error) => {
+      console.log('Problem loading data', error)
+    }).finally(() => { process.exit() })
+  }
+}
 
+const fileName    = path.resolve(__dirname, './masterList.csv')
+const parser      = parse({delimiter: ',', columns: true})
+const fileStream  = createReadStream(fileName)
+
+const handler = new MasterListHandler()
+
+fileStream.pipe(parser)
+  .on('data', handler.handleData)
+  .on('end', handler.handleEnd)
 
 console.log('at the end of the hallway')
