f46732932dc29bffdd3c44bdb8b394d86bbabb49
Austin Flores <ctrl4ltdeleteme@gmail.com>
2018-11-29 16:17:49 +0000

    Add comment for jest setup

--- back/jestSetup.ts
+++ back/jestSetup.ts
@@ -5,8 +5,10 @@ process.env.NODE_ENV = 'test'
 mongoose.set('debug')
 mongoose.Promise = Bluebird
 
+// https://medium.com/@art.longbottom.jr/concurrent-testing-with-mongoose-and-jest-83a27ceb87ee
 
 /*
+
   Define clearDB function that will loop through all
   the collections in our mongoose connection and drop them.
 */
@@ -19,7 +21,6 @@ function clearDB(mongoose, done) {
 
 
 beforeEach(function(done) {
-
   /*
     If the mongoose connection is closed,
     start it up using the test url and database name
