1ac4c9f7db3888a7fcb7a3e67260e4d297abf783
Austin Flores <ctrl4ltdeleteme@gmail.com>
2019-01-11 01:19:55 +0100

    Add node env to jest
    https://mongoosejs.com/docs/jest.html

--- back/jest.config.js
+++ back/jest.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  "verbose": true,
-  "roots": [
-    "<rootDir>/"
-  ],
-  "transform": {
-    "^.+\\.ts$": "ts-jest"
+  verbose: true,
+  roots: ['<rootDir>/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
-  "setupTestFrameworkScriptFile": "<rootDir>/jestSetup.ts",
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
-  "moduleFileExtensions": [
-    "ts",
-    "js",
-  ],
+  testEnvironment: 'node',
+  setupTestFrameworkScriptFile: '<rootDir>/jestSetup.ts',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js'],
 }
