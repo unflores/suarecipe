1e411fcfa3e34dc467dcb23bb807d64148764e5e
Austin Flores <ctrl4ltdeleteme@gmail.com>
2019-01-13 23:40:22 +0100

    test

--- front/webpack/webpack.prod.js
+++ front/webpack/webpack.prod.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-
+console.log('TESTING HERP DERP. LARK MARK. CLARK.')
 module.exports = merge(common, {
   mode: 'production',
 })
