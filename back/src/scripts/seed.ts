bf56d3f6359e2132ade8e2b48827ed963379919d
Austin Flores <ctrl4ltdeleteme@gmail.com>
2019-01-15 16:50:58 +0100

    Update error handling for incoming csv

--- back/src/scripts/seed.ts
+++ back/src/scripts/seed.ts
@@ -18,9 +18,14 @@ interface IImportedLocation {
   Price: number
 }
 
+interface IErrorListing {
+  error_listing: string
+  message: string
+}
+
 class MasterListHandler {
   public tasks: Array<Bluebird<void | Document>>
-  public errors: string[]
+  public errors: IErrorListing[]
 
   constructor() {
     this.tasks = []
@@ -41,23 +46,28 @@ class MasterListHandler {
 
     this.tasks.push(
       location.save().catch((err: Error) => {
-        const objectErrors: string[] = []
-        objectErrors.push(`Error listing: ${imported.Name}`)
-        objectErrors.push(`message: ${err.message}`)
-        objectErrors.push('================')
-
-        this.errors.push(objectErrors.join('\n'))
+        this.errors.push({
+          error_listing: imported.Name,
+          message: err.message,
+        })
       }),
     )
+  }
+
+  public handleError = (error): void => {
+    logger.error(error)
   }
 
   public handleEnd = (): void => {
     Bluebird.all(this.tasks)
       .then((data) => {
-        logger.info(`Errors: ${this.errors.join('\n')}`)
-        logger.info(`Total rows: ${this.tasks.length}`)
-        logger.info(`Total errors: ${this.errors.length}`)
-        logger.info(`Total saved: ${this.tasks.length - this.errors.length}`)
+        this.errors.forEach((error) => {
+          logger.info({ error: error })
+        })
+        logger.info({
+          total_rows: this.tasks.length,
+          total_saved: this.tasks.length - this.errors.length,
+        })
       })
       .catch((error) => {
         logger.info('Problem loading data', error)
@@ -69,7 +79,11 @@ class MasterListHandler {
 }
 
 const fileName = path.resolve(__dirname, './masterList.csv')
-const parser = parse({ delimiter: ',', columns: true })
+const parser = parse({
+  delimiter: ',',
+  columns: true,
+  relax_column_count: true,
+})
 const fileStream = createReadStream(fileName)
 
 const handler = new MasterListHandler()
