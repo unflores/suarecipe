1d25bd4384a3f4783736a761805fea643266d131
Austin Flores <ctrl4ltdeleteme@gmail.com>
2019-12-29 23:13:54 +0100

    Add basic routing

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -3,23 +3,37 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import reducer from './reducers/index'
 
 const store = createStore(
   reducer,
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
 import ItineraryBuilder from './ItineraryBuilder'
 
 const App = () => (
   <Provider store={store}>
-    <div>
-      <Main>
-        <ItineraryBuilder />
-      </Main>
-    </div>
+    <Router>
+      <div>
+        <Main>
+          <Switch>
+            <Route path="/builder">
+              <ItineraryBuilder />
+            </Route>
+            <Route
+              path="/admin/locations"
+              render={() => <div>Locations Page</div>}
+            />
+            <Route>
+              <ItineraryBuilder />
+            </Route>
+          </Switch>
+        </Main>
+      </div>
+    </Router>
   </Provider>
 )
 
