ecd951172eb35b754aa9ece2d416f0b000273627
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-02-24 00:27:20 +0100

    Add validation and update

--- back/src/controllers/locations.ts
+++ back/src/controllers/locations.ts
@@ -1,6 +1,11 @@
 import { Request, Response, Router } from 'express'
+import * as Joi from '@hapi/joi'
 import Location from '../models/location'
 
+const schema = Joi.object({
+  name: Joi.string(),
+  zipcode: Joi.number()
+})
 
 const router = Router()
 
@@ -10,7 +15,18 @@ router.get('/', async function (req: Request, res: Response) {
 })
 
 router.patch('/:id', async function (req: Request, res: Response) {
-  res.send(req.body)
+  const body = schema.validate(req.body)
+  if (body.error) {
+    return res.status(400).json({ error: body.error });
+  }
+
+  const location = await Location.findById(req.params.id)
+
+  location.set(body.value).save().then(
+    _ => res.send({ location })
+  ).catch(
+    result => res.status(400).send(result)
+  )
 })
 
 export default router;
