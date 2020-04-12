ee6bddd4741125ed517a6f9e89ade87d6f4881a0
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-04-12 14:09:04 +0200

    Fix ingredient controller spec

--- back/src/controllers/__test__/ingredients.spec.ts
+++ back/src/controllers/__test__/ingredients.spec.ts
@@ -1,15 +1,33 @@
 import app from '../../server'
 import * as request from 'supertest'
+import Ingredient, { IIngredientModel } from '../../models/ingredient'
+import { expect } from 'chai'
 
 const server = request(app)
 
 describe('ingredients', () => {
   describe('get', () => {
+    let ingredient: IIngredientModel
+
+    beforeEach(async () => {
+      ingredient = await Ingredient.create({
+        name: 'something',
+        zipcode: 75010,
+        type: 'thing',
+        partsOfDay: 'afternoon'
+      })
+    })
+
     it('responds with a ingredient array', async () => {
       await server
         .get('/api/ingredients/')
         .expect(200)
-        .expect('derp')
+        .then(response => {
+          const data = response.body[0]
+          expect(data.name).to.eql(ingredient.name)
+          expect(response.body.length).to.eql(1)
+        })
+
     })
   })
 })
