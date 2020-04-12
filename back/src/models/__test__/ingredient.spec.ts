ebd305e295daa5401cb87febbcfdf55bd44914bf
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-04-12 14:01:01 +0200

    Remove more old stuff

--- back/src/models/__test__/ingredient.spec.ts
+++ back/src/models/__test__/ingredient.spec.ts
@@ -14,13 +14,6 @@ describe('Ingredient', () => {
   it('name is unique', (done) => {
     const params: IIngredient = {
       name: 'name',
-      type: 'type',
-      partsOfDay: ['night'],
-      description: 'description',
-      siteLink: 'http://derp.com',
-      price: 38,
-      address: 'street',
-      zipcode: 75010,
     }
     const ingredient = new Ingredient(params)
 
@@ -31,36 +24,6 @@ describe('Ingredient', () => {
         expect(error1.errors.name).not.to.be.an('undefined')
         done()
       })
-    })
-  })
-
-  it('requires type', (done) => {
-    subject.validate((error) => {
-      expect(error.errors.type).not.to.be.an('undefined')
-      done()
-    })
-  })
-
-  it('requires zipcode', (done) => {
-    subject.validate((error) => {
-      expect(error.errors.zipcode).not.to.be.an('undefined')
-      done()
-    })
-  })
-
-  it('requires partsOfDay to have values from dayParts enum', (done) => {
-    const ingredient = new Ingredient({ partsOfDay: ['derply'] })
-    ingredient.validate((error) => {
-      expect(error.errors.partsOfDay).not.to.be.an('undefined')
-      done()
-    })
-  })
-
-  it('requires siteLink to be a url', (done) => {
-    const ingredient = new Ingredient({ siteLink: 'derply' })
-    ingredient.validate((error) => {
-      expect(error.errors.siteLink).not.to.be.an('undefined')
-      done()
     })
   })
 })
--- back/src/server/router.ts
+++ back/src/server/router.ts
@@ -1,7 +1,5 @@
-import itinerariesController from '../controllers/itineraries'
 import ingredientsController from '../controllers/ingredients'
 
 export default (app) => {
-  app.use('/api/itineraries', itinerariesController)
   app.use('/api/ingredients', ingredientsController)
 }
