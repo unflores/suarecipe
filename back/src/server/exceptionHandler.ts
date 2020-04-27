8e58837eb5bec16c692fa60758aae1693b0ef137
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-04-27 23:35:32 +0200

    Add exceptionhandlers

--- back/src/server/exceptionHandler.ts
+++ back/src/server/exceptionHandler.ts
@@ -1,17 +1,27 @@
 
-import { Express } from 'express'
+import { Express, Response, Request } from 'express'
 
 
 
 export default (app: Express) => {
 
-  app.use(function handleParamsError(error: Error, req, res, next) {
+  app.use(function handleParamsError(error: Error, req: Request, res: Response, next) {
     if (error.name !== "ParamsError") {
       next(error)
     }
+    console.log(error)
 
     return res.status(400).json({
       type: 'ParamsError',
+      message: error.message
+    })
+  })
+
+  app.use(function handleUnkown(error: Error, req: Request, res: Response, next) {
+    console.log(error)
+
+    return res.status(500).json({
+      type: 'Unknown error',
       message: error.message
     })
   })
