545f8216b410e39cec170290acf401785835057b
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-08-20 16:34:29 +0200

    Add some code to play around with search

--- front/app/admin/Recipes/index.tsx
+++ front/app/admin/Recipes/index.tsx
@@ -5,12 +5,18 @@ import { connect } from 'react-redux'
 import api from 'frontapp/api'
 import { Recipe, RecipesResponse } from 'frontapp/libs/api/Responses'
 import AdminTable from 'frontapp/rcl/AdminTable'
+import Search from 'frontapp/rcl/Search'
 import { recipesFetched } from 'frontapp/reducers/recipes/actionBuilders'
 import { Dispatch } from 'redux'
 
 interface Props {
   recipes: Recipe[]
   onFetchRecipes: (response: RecipesResponse) => void
+}
+
+const buildRecipes = (amount: number) => {
+  return Array.from(Array(amount)).map((_x, i) => ({ value: `Some recipe ${i}`, id: `${i}` })
+  );
 }
 
 class Recipes extends React.Component<Props, {}> {
@@ -25,6 +31,12 @@ class Recipes extends React.Component<Props, {}> {
     return (
       <>
         <h3>Recipes</h3>
+
+        <Search
+          onSearch={() => buildRecipes(20)}
+          onSelect={(id) => { console.log(id) }}
+        />
+
         <AdminTable
           headers={['Name', '']}
         >
