a6b50526b385bd25a664015804ca97e522cce5a4
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-10-25 23:54:39 +0100

    Fix more linting

--- front/app/admin/EditRecipe/index.tsx
+++ front/app/admin/EditRecipe/index.tsx
@@ -1,27 +1,37 @@
-import * as React from 'react'
-import Form from 'frontapp/rcl/Form'
-import BasicInput from 'frontapp/rcl/Atoms/BasicInput'
-import DropdownSearch from 'frontapp/rcl/Search/DropdownSearch'
+import api, { mergeIntersect } from 'frontapp/api'
 import { Recipe, RecipeResponse } from 'frontapp/libs/api/Responses'
-import api from 'frontapp/api'
-import { Ingredient, IngredientsResponse } from 'frontapp/libs/api/Responses'
-import Dropdown from 'frontapp/rcl/Atoms/DropDown'
+import { Ingredient, IngredientsResponse, UsedIngredient } from 'frontapp/libs/api/Responses'
+import BasicInput from 'frontapp/rcl/Atoms/BasicInput'
+import Form from 'frontapp/rcl/Form'
+import DropdownSearch from 'frontapp/rcl/Search/DropdownSearch'
+import * as React from 'react'
+
+import UsedIngredientInput from 'frontapp/rcl/Molecules/UsedIngredientInput'
+
+interface SearchResult {
+  id: string
+  value: string
+}
+
+interface UsedIngredientNames {
+  [id: string]: string
+}
+
+const transformIngredientsToSearchResults = (ingredients: Ingredient[]): SearchResult[] => {
+  return ingredients.map((ingredient) => ({
+    id: ingredient._id,
+    value: ingredient.name,
+  }))
+}
 
 interface Props {
   recipeId: string
 }
 
-interface Ingredient {
-  ingredientId: string
-  quantity: number
-  measurement: string
-}
-
 interface State {
-  recipe: {
-    name: string,
-    usedIngredients: Ingredient[]
-  }
+  recipeAtts: Recipe
+  usedIngredientNames: UsedIngredientNames
+  searchResults: SearchResult[]
 }
 
 class EditRecipe extends React.Component<Props, State> {
@@ -29,31 +39,93 @@ class EditRecipe extends React.Component<Props, State> {
     super(props)
 
     this.state = {
-      recipe: {
+      usedIngredientNames: {},
+      recipeAtts: {
+        _id: '',
         name: '',
         usedIngredients: [],
-
-      }
+      },
+      searchResults: [],
     }
   }
 
-  handleSubmit = () => {
-
+  findUsedIngredientName = (id: string) => {
+    const result = this.state.searchResults.find((searchResult) => searchResult.id === id)
+    return result
   }
 
-  updateObject = (event: React.FormEvent<HTMLInputElement>) => {
-    const { value, name } = event.currentTarget
-    const stateChange: { [k: string]: string } = {}
-    stateChange[name] = value
+  async componentDidMount() {
+    const recipesResponse = await api.get<RecipeResponse>(`/api/recipes/${this.props.recipeId}`)
 
+    this.setState({ recipeAtts: mergeIntersect(this.state.recipeAtts, recipesResponse.data.recipe) })
+  }
+
+  handleSubmit = async () => {
+    const { _id, ...atts } = this.state.recipeAtts
+    const response = await api.put<Recipe>(`/api/recipes/${_id}`, atts)
+
+    if (response.code >= 400) {
+      console.log("There was an error setting your information")
+    }
+  }
+
+  handleSelectSearchItem = (id: string) => {
+    const name = this.findUsedIngredientName(id)
+    const usedIngredient = { ingredientId: id, quantity: 0, measurement: '' }
     this.setState({
-      recipe: { ...this.state.recipe, ...stateChange }
+      recipeAtts: {
+        ...this.state.recipeAtts,
+        usedIngredients: [...this.state.recipeAtts.usedIngredients, usedIngredient],
+      },
+      usedIngredientNames: { ...this.state.usedIngredientNames, [name.id]: name.value },
     })
   }
 
-  render() {
-    const { recipe } = this.state
+  handleRemoveIngredient = (id: string) => {
+    const { usedIngredients } = this.state.recipeAtts
+    this.setState({
+      recipeAtts: {
+        ...this.state.recipeAtts,
+        usedIngredients: usedIngredients.filter((usedIngredient) => id !== usedIngredient.ingredientId),
+      },
+    })
+  }
 
+  searchRecipe = async (searchText: string) => {
+    const response = await api.get<IngredientsResponse>('/api/ingredients/',
+      { search: searchText },
+    )
+    const searchResults = transformIngredientsToSearchResults(response.data.ingredients)
+    this.setState({ searchResults })
+    return searchResults
+  }
+
+  updateObject = ({ name, value }: { name: string, value: string }) => {
+    this.setState({
+      recipeAtts: { ...this.state.recipeAtts, [name]: value },
+    })
+  }
+
+  updateUsedIngredient = (ingredient: UsedIngredient) => {
+    const { usedIngredients } = this.state.recipeAtts
+
+    const index = usedIngredients.findIndex(
+      (usedIngredient) => usedIngredient.ingredientId === ingredient.ingredientId,
+    )
+    const newUsedIngredients = [
+      ...usedIngredients.slice(0, index),
+      ingredient,
+      ...usedIngredients.slice(index + 1, usedIngredients.length),
+    ]
+
+    this.setState({
+      recipeAtts: { ...this.state.recipeAtts, usedIngredients: newUsedIngredients },
+    })
+    console.log({ recipeAtts: { ...this.state.recipeAtts, usedIngredients: newUsedIngredients } })
+  }
+
+  render() {
+    const { recipeAtts, usedIngredientNames } = this.state
     return (
       <>
         <Form
@@ -61,11 +133,27 @@ class EditRecipe extends React.Component<Props, State> {
         >
           <BasicInput
             labelText="Name: "
-            id={recipe.name}
+            id={recipeAtts.name}
             name="name"
-            value={recipe.name}
+            value={recipeAtts.name}
             onChange={this.updateObject}
           />
+          <DropdownSearch
+            onSearch={this.searchRecipe}
+            onSelect={this.handleSelectSearchItem}
+          />
+
+          <div>
+            {recipeAtts.usedIngredients.map((usedIngredient: UsedIngredient) =>
+              <UsedIngredientInput
+                name={usedIngredientNames[usedIngredient.ingredientId]}
+                _id={usedIngredient.ingredientId}
+                key={usedIngredient.ingredientId}
+                onRemove={this.handleRemoveIngredient}
+                onChange={this.updateUsedIngredient}
+              />,
+            )}
+          </div>
         </Form >
       </>
     )
