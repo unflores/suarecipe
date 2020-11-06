2fe1cc40a5133d716145c0b6bd555f62f95b9000
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-11-07 00:09:24 +0100

    Add inspect to repl

--- back/src/scripts/repl.ts
+++ back/src/scripts/repl.ts
@@ -1,13 +1,18 @@
-import * as repl from 'repl'
 import * as dotenv from 'dotenv'
+import * as repl from 'repl'
+import * as util from 'util'
+
 dotenv.config()
 import { dbSetup } from '../../config/mongoose'
 dbSetup()
 
 const replServer = repl.start('> ')
-import { Ingredient } from 'src/models/ingredient'
-import { Recipe } from 'src/models/recipe'
+import { Ingredient } from '../models/ingredient'
+import { Recipe } from '../models/recipe'
 
+replServer.context.show = (thing) => {
+  console.log(util.inspect(thing, false, null, true))
+}
 replServer.context.Ingredient = Ingredient
 replServer.context.Recipe = Recipe
 /* tslint:disable: no-console*/
