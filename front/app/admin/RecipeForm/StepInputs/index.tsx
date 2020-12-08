3c080f1c6b836b72cb877c0467b603c09598cb14
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-12-08 23:24:03 +0100

    Remove drag and drop code from StepInputs

--- front/app/admin/RecipeForm/StepInputs/index.tsx
+++ front/app/admin/RecipeForm/StepInputs/index.tsx
@@ -16,52 +16,7 @@ interface State {
 
 }
 
-const getItemStyle = (isDragging: boolean, draggableStyle: object) => ({
-  display: 'flex',
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
-  background: isDragging ? "lightgreen" : "#fff",
-  // styles we need to apply on draggables
-  ...draggableStyle
-})
-
-const getListStyle = (isDraggingOver: boolean) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
-  width: `40rem`
-})
-
-const grid = 8
-
-const reorder = (steps: Step[], startIndex: number, endIndex: number) => {
-  const changedSteps = Array.from(steps)
-  const [removed] = changedSteps.splice(startIndex, 1)
-  changedSteps.splice(endIndex, 0, removed)
-
-  return changedSteps
-}
-
-const draggedOutsideDroppable = ({ destination }: DropResult) => {
-  return !destination
-}
-
 class StepInputs extends React.Component<Props, State> {
-
-  onDragEnd = (result: DropResult) => {
-
-    if (draggedOutsideDroppable(result)) {
-      return
-    }
-
-    const steps = reorder(
-      this.props.steps,
-      result.source.index,
-      result.destination.index
-    )
-
-    this.props.onChange(steps)
-
-  }
 
   handleRemoveIngredient = (step: Step) => {
     const steps = Array.from(this.props.steps)
