9b0cc596e22f2ada63c8a370f94835dd3b2549bc
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-01-16 16:44:30 +0100

    Add show page for recipes

--- front/app/sites/BookSite.tsx
+++ front/app/sites/BookSite.tsx
@@ -1,17 +1,28 @@
 import MainLayout from 'frontapp/templates/Main'
 import * as React from 'react'
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, RouteComponentProps } from "react-router-dom"
 import ErrorBoundary from '../ErrorBoundary'
 import MainRecipes from '../main/Recipes'
+import Recipe from '../main/Recipe'
+
+interface RecipeProps extends RouteComponentProps<{ recipeId: string }> { }
 
 const Book = () => (
   <MainLayout>
     <Switch>
-      <Route path="/book/recipes">
+      <Route exact path="/book/recipes">
         <ErrorBoundary key="/recipes">
           <MainRecipes />
         </ErrorBoundary>
       </Route>
+      <Route
+        path="/book/recipes/:recipeId/"
+        render={(props: RecipeProps) =>
+          <ErrorBoundary key={props.match.url}>
+            <Recipe recipeId={props.match.params.recipeId} />
+          </ErrorBoundary>
+        }
+      />
       <ErrorBoundary key="/recipes">
         <MainRecipes />
       </ErrorBoundary>
