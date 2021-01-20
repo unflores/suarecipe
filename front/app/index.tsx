3ede3b50163ef4a1824cacf957872c63ca738f49
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-01-20 14:03:49 +0100

    Send redirect to proper place

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -25,7 +25,7 @@ const App = () => (
           <Route path="/admin">
             <AdminSite />
           </Route>
-          <Redirect to="/book/recipe" />
+          <Redirect to="/book/recipes" />
         </Switch>
       </div>
     </Router >
