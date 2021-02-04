f6258341ac5201e3d2709c0e0e4d950263ddcaf6
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-02-04 23:40:39 +0100

    Updated how the specs are done

--- back/src/models/__test__/ingredient.spec.ts
+++ back/src/models/__test__/ingredient.spec.ts
@@ -1,29 +1,11 @@
 import { expect } from 'chai'
-import { Ingredient, IIngredient } from '../ingredient'
+import { Ingredient } from '../ingredient'
 
 describe('Ingredient', () => {
   const subject = new Ingredient({})
 
-  it('requires name', (done) => {
-    subject.validate((error) => {
-      expect(error.errors.name).not.to.be.an('undefined')
-      done()
-    })
-  })
-
-  it('name is unique', (done) => {
-    const params: IIngredient = {
-      name: 'name',
-    }
-    const ingredient = new Ingredient(params)
-
-    ingredient.save((error) => {
-      expect(error).to.be.an('null')
-      const ingredient2 = new Ingredient(params)
-      ingredient2.save((error1) => {
-        expect(error1.errors.name).not.to.be.an('undefined')
-        done()
-      })
-    })
+  it('requires name', () => {
+    subject.validateSync()
+    expect(subject.errors.name).not.to.be.an('undefined')
   })
 })
