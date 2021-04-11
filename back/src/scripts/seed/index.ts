2d83378e1e6b7118b8b0a82cafdba8ad06c21cb1
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-04-11 14:15:39 +0200

    Clean up seed file

--- back/src/scripts/seed/index.ts
+++ back/src/scripts/seed/index.ts
@@ -3,9 +3,10 @@ import database, { dbSetup } from '../../../config/mongoose'
 
 dbSetup()
 
-import { MasterListHandler } from './masterListHandler'
+// import { MasterListHandler } from './masterListHandler'
 import { createReadStream } from 'fs'
 import * as path from 'path'
+import { Ingredient } from '../../models/ingredient'
 import { Recipe } from '../../models/recipe'
 
 const fileName = path.resolve(__dirname, '../masterList.csv')
@@ -16,26 +17,69 @@ const csvParser = parse({
 })
 const fileStream = createReadStream(fileName)
 
-const handler = new MasterListHandler()
+interface Ingredient {
+  name: string
+}
 
-Object.keys(database.connection.collections).forEach((collectionId) => {
-  database.connection.collections[collectionId].deleteMany({})
-})
+const extractedIngredients: Ingredient[] = []
 
-fileStream
-  .pipe(csvParser)
-  .on('data', handler.handleData)
-  .on('end', handler.handleEnd)
+const seedDb = async () => {
+  await clearDBCollections()
 
-const recipes = [
-  new Recipe({
-    name: "Just butter"
-  }),
-  new Recipe({
-    name: "Chicken too"
-  }),
-]
+  fileStream
+    .pipe(csvParser)
+    .on('data', addIngredientParamsToExtracted)
+    .on('end', async () => {
+      await createIngredients(extractedIngredients)
+      await createRecipes()
+      process.exit()
+    })
+}
 
-recipes.map(async (recipe, index) => {
-  await recipe.save()
-})
+const clearDBCollections = async () => {
+  Object.keys(database.connection.collections).forEach(async (collectionId) => {
+    await database.connection.collections[collectionId].deleteMany({})
+  })
+}
+
+const addIngredientParamsToExtracted =
+  (imported: { Name: string }) => extractedIngredients.push({ name: imported.Name })
+
+const createIngredients = async (ingredients: Ingredient[]) => {
+  await Ingredient.collection.insertMany(ingredients)
+}
+
+const createRecipes = async () => {
+  const [chicken, butter, salt] = await Ingredient.where(
+    { name: { $in: ['Chicken Thigh', 'Butter', 'Salt'] } }
+  )
+
+  const recipes = [
+    new Recipe({
+      name: "Just butter",
+      steps: [{ body: 'Put butter in a pan' }, { body: 'Stir it around...I have no idea' }]
+    }),
+    new Recipe({
+      name: "Chicken on plate",
+      usedIngredients: [
+        { ingredient: chicken.id, quantity: 1, measurement: 'pound' },
+        { ingredient: butter.id, quantity: 1, measurement: 'dollup' },
+        { ingredient: salt.id, quantity: 2, measurement: 'teaspoons' },
+      ],
+      steps: [
+        { body: 'Put that butter in the pan' },
+        { body: 'Put Chicken in pan' },
+        { body: 'Cook it for like 5 mins on high' },
+
+      ]
+    }),
+  ]
+
+  await Promise.all(
+    recipes.map(async (recipe) => {
+      await recipe.save()
+    })
+  )
+}
+
+seedDb()
